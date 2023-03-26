825932f91337b34eb26171c26125773d21d1c817
Leon <tahjib.leon399@gmail.com>
2023-03-26 21:03:05 +0600

    Linear search

--- Linear-search.js
+++ Linear-search.js
@@ -1,1 +1,16 @@
-console.log('Linear search for DSA');
+let arr = [23, 34, 43, 2, 32, 54, 323];
+const target = 43;
+
+
+
+function linearSearch(arr, target) {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return `Target ${target} founded in index ${i}`
+        }
+    }
+    return 'not found'
+}
+
+console.log(linearSearch(arr, target));
