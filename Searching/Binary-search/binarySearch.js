212d19416d91693649a3efc4d21237829ea8cdbe
Leon <tahjib.leon399@gmail.com>
2023-09-25 23:22:49 +0600

    binary

---
+++ Searching/Binary-search/binarySearch.js
@@ -0,0 +1,16 @@
+let arr = [23, 34, 43, 2, 32, 54, 323];
+const target = 43;
+
+
+
+function linearSearch(arr, target) {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return `Target ${target} found in index ${i}`
+        }
+    }
+    return 'not found'
+}
+
+console.log(linearSearch(arr, target));
